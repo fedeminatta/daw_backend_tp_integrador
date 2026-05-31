ad1f5d3fdff56fdb243d974337f14fcffdd3b0ad
Mateo Acuña <matiu098@gmail.com>
2026-05-31 17:45:46 -0300

    configura conexion a bd postgresql con typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const host = configService.get<string>('DB_HOST');
+        const port = configService.get<number>('DB_PORT');
+        const username = configService.get<string>('DB_USER');
+        const password = configService.get<string>('DB_PASSWORD');
+        const database = configService.get<string>('DB_NAME');
+
+        if (!host || !port || !username || !password || !database) {
+          throw new Error(
+            'Faltan variables de entorono para la configuracion de la base de datos postgresql.',
+          );
+        }
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+          autoLoadEntities: true,
+          synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
