0d12bfb970ba526fd2b1b6a13f04c93044275fc3
Mateo Acuña <matiu098@gmail.com>
2026-06-03 00:58:16 -0300

    configuracion entidad de tareas y resolucion  dependencias de modulos

--- src/tareas/tareas.module.ts
+++ src/tareas/tareas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TareasService } from './tareas.service';
 import { TareasController } from './tareas.controller';
+import { Tarea } from './entities/tarea.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tarea])],
   controllers: [TareasController],
   providers: [TareasService],
 })
