f834a087d648b86f326c4468f04d6bfa8ae07e97
fedeminatta <minattafederico@gmail.com>
2026-06-03 19:53:52 -0300

    crear usuario por defectto si no hay

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario, UsuarioEstado } from './entities/usuario.entity';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
 @Injectable()
-export class UsuariosService {
+export class UsuariosService implements OnModuleInit {
+  constructor(
+    @InjectRepository(Usuario)
+    private readonly usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async onModuleInit() {
+    await this.crearUsuarioPorDefecto();
+  }
+
+  private async crearUsuarioPorDefecto() {
+    // ver si existe algun usuario en la base de datos
+    const cantidadUsuarios = await this.usuarioRepository.count();
+
+    if (cantidadUsuarios === 0) {
+      console.log('No se encontraron usuarios. Creando usuario por defecto...');
+
+      const usuarioAdmin = this.usuarioRepository.create({
+        nombreUsuario: 'admin',
+        clave: 'admin123',
+        estado: UsuarioEstado.ACTIVO,
+      });
+
+      await this.usuarioRepository.save(usuarioAdmin);
+      console.log(
+        '✅ Usuario por defecto creado con éxito -> Usuario: admin | Clave: admin123',
+      );
+    } else {
+      console.log('ℹ️ La base de datos ya tiene usuarios cargados.');
+    }
+  }
+
   create(createUsuarioDto: CreateUsuarioDto) {
     return 'This action adds a new usuario';
   }
